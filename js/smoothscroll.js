6e8b454ab613fadc1bb4586928e9c88ca3931b22
Ricky <rickyliew@hotmail.com>
2020-03-12 17:54:34 +1100

    smooth scroll

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -1,33 +1,160 @@
-/**
- * SmoothScroll
- * This helper script created by DWUser.com.  Copyright 2013 DWUser.com.  
- * Dual-licensed under the GPL and MIT licenses.  
- * All individual scripts remain property of their copyrighters.
- * Date: 10-Sep-2013
- * Version: 1.0.1
- */
-if (!window['jQuery']) alert('The jQuery library must be included before the smoothscroll.js file.  The plugin will not work propery.');
+;(function( $ ){
 
-/**
- * jQuery.ScrollTo - Easy element scrolling using jQuery.
- * Copyright (c) 2007-2013 Ariel Flesler - aflesler(at)gmail(dot)com | http://flesler.blogspot.com
- * Dual licensed under MIT and GPL.
- * @author Ariel Flesler
- * @version 1.4.3.1
- */
-;(function($){var h=$.scrollTo=function(a,b,c){$(window).scrollTo(a,b,c)};h.defaults={axis:'xy',duration:parseFloat($.fn.jquery)>=1.3?0:1,limit:true};h.window=function(a){return $(window)._scrollable()};$.fn._scrollable=function(){return this.map(function(){var a=this,isWin=!a.nodeName||$.inArray(a.nodeName.toLowerCase(),['iframe','#document','html','body'])!=-1;if(!isWin)return a;var b=(a.contentWindow||a).document||a.ownerDocument||a;return/webkit/i.test(navigator.userAgent)||b.compatMode=='BackCompat'?b.body:b.documentElement})};$.fn.scrollTo=function(e,f,g){if(typeof f=='object'){g=f;f=0}if(typeof g=='function')g={onAfter:g};if(e=='max')e=9e9;g=$.extend({},h.defaults,g);f=f||g.duration;g.queue=g.queue&&g.axis.length>1;if(g.queue)f/=2;g.offset=both(g.offset);g.over=both(g.over);return this._scrollable().each(function(){if(e==null)return;var d=this,$elem=$(d),targ=e,toff,attr={},win=$elem.is('html,body');switch(typeof targ){case'number':case'string':if(/^([+-]=)?\d+(\.\d+)?(px|%)?$/.test(targ)){targ=both(targ);break}targ=$(targ,this);if(!targ.length)return;case'object':if(targ.is||targ.style)toff=(targ=$(targ)).offset()}$.each(g.axis.split(''),function(i,a){var b=a=='x'?'Left':'Top',pos=b.toLowerCase(),key='scroll'+b,old=d[key],max=h.max(d,a);if(toff){attr[key]=toff[pos]+(win?0:old-$elem.offset()[pos]);if(g.margin){attr[key]-=parseInt(targ.css('margin'+b))||0;attr[key]-=parseInt(targ.css('border'+b+'Width'))||0}attr[key]+=g.offset[pos]||0;if(g.over[pos])attr[key]+=targ[a=='x'?'width':'height']()*g.over[pos]}else{var c=targ[pos];attr[key]=c.slice&&c.slice(-1)=='%'?parseFloat(c)/100*max:c}if(g.limit&&/^\d+$/.test(attr[key]))attr[key]=attr[key]<=0?0:Math.min(attr[key],max);if(!i&&g.queue){if(old!=attr[key])animate(g.onAfterFirst);delete attr[key]}});animate(g.onAfter);function animate(a){$elem.animate(attr,f,g.easing,a&&function(){a.call(this,e,g)})}}).end()};h.max=function(a,b){var c=b=='x'?'Width':'Height',scroll='scroll'+c;if(!$(a).is('html,body'))return a[scroll]-$(a)[c.toLowerCase()]();var d='client'+c,html=a.ownerDocument.documentElement,body=a.ownerDocument.body;return Math.max(html[scroll],body[scroll])-Math.min(html[d],body[d])};function both(a){return typeof a=='object'?a:{top:a,left:a}}})(jQuery);
-
-/**
- * jQuery.LocalScroll
- * Copyright (c) 2007-2010 Ariel Flesler - aflesler(at)gmail(dot)com | http://flesler.blogspot.com
- * Dual licensed under MIT and GPL.
- * Date: 05/31/2010
- * @author Ariel Flesler
- * @version 1.2.8b
- **/
-;(function(b){function g(a,e,d){var h=e.hash.slice(1),f=document.getElementById(h)||document.getElementsByName(h)[0];if(f){a&&a.preventDefault();var c=b(d.target);if(!(d.lock&&c.is(":animated")||d.onBefore&&!1===d.onBefore(a,f,c))){d.stop&&c._scrollable().stop(!0);if(d.hash){var a=f.id==h?"id":"name",g=b("<a> </a>").attr(a,h).css({position:"absolute",top:b(window).scrollTop(),left:b(window).scrollLeft()});f[a]="";b("body").prepend(g);location=e.hash;g.remove();f[a]=h}c.scrollTo(f,d).trigger("notify.serialScroll",
-[f])}}}var i=location.href.replace(/#.*/,""),c=b.localScroll=function(a){b("body").localScroll(a)};c.defaults={duration:1E3,axis:"y",event:"click",stop:!0,target:window,reset:!0};c.hash=function(a){if(location.hash){a=b.extend({},c.defaults,a);a.hash=!1;if(a.reset){var e=a.duration;delete a.duration;b(a.target).scrollTo(0,a);a.duration=e}g(0,location,a)}};b.fn.localScroll=function(a){function e(){return!!this.href&&!!this.hash&&this.href.replace(this.hash,"")==i&&(!a.filter||b(this).is(a.filter))}
-a=b.extend({},c.defaults,a);return a.lazy?this.bind(a.event,function(d){var c=b([d.target,d.target.parentNode]).filter(e)[0];c&&g(d,c,a)}):this.find("a,area").filter(e).bind(a.event,function(b){g(b,this,a)}).end().end()}})(jQuery);
-
-// Initialize all .smoothScroll links
-jQuery(function($){ $.localScroll({filter:'.smoothScroll'}); });
+  var $scrollTo = $.scrollTo = function( target, duration, settings ){
+      $(window).scrollTo( target, duration, settings );
+  };
+  
+  $scrollTo.defaults = {
+      axis:'xy',
+      duration: parseFloat($.fn.jquery) >= 1.3 ? 0 : 1
+  };
+  
+  // Returns the element that needs to be animated to scroll the window.
+  // Kept for backwards compatibility (specially for localScroll & serialScroll)
+  $scrollTo.window = function( scope ){
+      return $(window)._scrollable();
+  };
+  
+  // Hack, hack, hack :)
+  // Returns the real elements to scroll (supports window/iframes, documents and regular nodes)
+  $.fn._scrollable = function(){
+      return this.map(function(){
+          var elem = this,
+              isWin = !elem.nodeName || $.inArray( elem.nodeName.toLowerCase(), ['iframe','#document','html','body'] ) != -1;
+  
+              if( !isWin )
+                  return elem;
+  
+          var doc = (elem.contentWindow || elem).document || elem.ownerDocument || elem;
+  
+          return $.browser.safari || doc.compatMode == 'BackCompat' ?
+              doc.body : 
+              doc.documentElement;
+      });
+  };
+  
+  $.fn.scrollTo = function( target, duration, settings ){
+      if( typeof duration == 'object' ){
+          settings = duration;
+          duration = 0;
+      }
+      if( typeof settings == 'function' )
+          settings = { onAfter:settings };
+  
+      if( target == 'max' )
+          target = 9e9;
+  
+      settings = $.extend( {}, $scrollTo.defaults, settings );
+      // Speed is still recognized for backwards compatibility
+      duration = duration || settings.speed || settings.duration;
+      // Make sure the settings are given right
+      settings.queue = settings.queue && settings.axis.length > 1;
+  
+      if( settings.queue )
+          // Let's keep the overall duration
+          duration /= 60000;
+      settings.offset = both( settings.offset );
+      settings.over = both( settings.over );
+  
+      return this._scrollable().each(function(){
+          var elem = this,
+              $elem = $(elem),
+              targ = target, toff, attr = {},
+              win = $elem.is('html,body');
+  
+          switch( typeof targ ){
+              // A number will pass the regex
+              case 'number':
+              case 'string':
+                  if( /^([+-]=)?\d+(\.\d+)?(px|%)?$/.test(targ) ){
+                      targ = both( targ );
+                      // We are done
+                      break;
+                  }
+                  // Relative selector, no break!
+                  targ = $(targ,this);
+              case 'object':
+                  // DOMElement / jQuery
+                  if( targ.is || targ.style )
+                      // Get the real position of the target 
+                      toff = (targ = $(targ)).offset();
+          }
+          $.each( settings.axis.split(''), function( i, axis ){
+              var Pos = axis == 'x' ? 'Left' : 'Top',
+                  pos = Pos.toLowerCase(),
+                  key = 'scroll' + Pos,
+                  old = elem[key],
+                  max = $scrollTo.max(elem, axis);
+  
+              if( toff ){// jQuery / DOMElement
+                  attr[key] = toff[pos] + ( win ? 0 : old - $elem.offset()[pos] );
+  
+                  // If it's a dom element, reduce the margin
+                  if( settings.margin ){
+                      attr[key] -= parseInt(targ.css('margin'+Pos)) || 0;
+                      attr[key] -= parseInt(targ.css('border'+Pos+'Width')) || 0;
+                  }
+  
+                  attr[key] += settings.offset[pos] || 0;
+  
+                  if( settings.over[pos] )
+                      // Scroll to a fraction of its width/height
+                      attr[key] += targ[axis=='x'?'width':'height']() * settings.over[pos];
+              }else{ 
+                  var val = targ[pos];
+                  // Handle percentage values
+                  attr[key] = val.slice && val.slice(-1) == '%' ? 
+                      parseFloat(val) / 100 * max
+                      : val;
+              }
+  
+              // Number or 'number'
+              if( /^\d+$/.test(attr[key]) )
+                  // Check the limits
+                  attr[key] = attr[key] <= 0 ? 0 : Math.min( attr[key], max );
+  
+              // Queueing axes
+              if( !i && settings.queue ){
+                  // Don't waste time animating, if there's no need.
+                  if( old != attr[key] )
+                      // Intermediate animation
+                      animate( settings.onAfterFirst );
+                  // Don't animate this axis again in the next iteration.
+                  delete attr[key];
+              }
+          });
+  
+          animate( settings.onAfter );            
+  
+          function animate( callback ){
+              $elem.animate( attr, duration, settings.easing, callback && function(){
+                  callback.call(this, target, settings);
+              });
+          };
+  
+      }).end();
+  };
+  
+  // Max scrolling position, works on quirks mode
+  // It only fails (not too badly) on IE, quirks mode.
+  $scrollTo.max = function( elem, axis ){
+      var Dim = axis == 'x' ? 'Width' : 'Height',
+          scroll = 'scroll'+Dim;
+  
+      if( !$(elem).is('html,body') )
+          return elem[scroll] - $(elem)[Dim.toLowerCase()]();
+  
+      var size = 'client' + Dim,
+          html = elem.ownerDocument.documentElement,
+          body = elem.ownerDocument.body;
+  
+      return Math.max( html[scroll], body[scroll] ) 
+           - Math.min( html[size]  , body[size]   );
+  
+  };
+  
+  function both( val ){
+      return typeof val == 'object' ? val : { top:val, left:val };
+  };
+  
+  })( jQuery );
